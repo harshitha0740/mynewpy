d23246811b2353632f1d279622ad1530249eea98
root <root@ip-172-31-12-75.ec2.internal>
2026-07-06 18:12:35 +0000

    updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Hello from the Docker + Jenkins CI/CD demo!",
+        "message": "Hello from the Docker + Jenkins CI/CD demo! welcome to deployment session and have a good day",
         "version": os.getenv("APP_VERSION", "1.0.0")
     })
 
